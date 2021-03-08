af08b8ec5884acfbfdd027bd8e94d1590ec3b6c3
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2021-03-08 12:32:14 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/g34/bl/recipedatabase/RecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g34.bl.recipedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
